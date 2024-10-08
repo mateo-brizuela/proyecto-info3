6e3a0a15c9dd0a3af01a4c16e53143582bf93998
Mateo Brizuela <mateobrizuela44@gmail.com>
2024-10-08 16:13:18 -0300

    esta bastante mas completo, podria decirse que es un abrol binario de busqueda funcional... aunque deberia probarse un poco mas

--- ProyectoJava/Practico8/ArbolAVL.java
+++ ProyectoJava/Practico8/ArbolAVL.java
@@ -3,8 +3,11 @@ package ProyectoJava.Practico8;
 import java.util.*;
 
 public class ArbolAVL {
-    NodoArbol root;
+    private NodoArbol root;
 
+    public NodoArbol getRoot(){
+        return root;
+    }
 
     public boolean isEmpty(){
         return root == null;
@@ -24,20 +27,54 @@ public class ArbolAVL {
         
     }
 
-    public void agregar(NodoArbol nuevoNodo){
+    public void agregar(){
+        Scanner consola = new Scanner(System.in);
+        NodoArbol nuevoNodo;
+        int nDato; // nuevo dato
+        System.out.println("ingrese el dato del nuevo nodo");
+        nDato = consola.nextInt();
+        nuevoNodo = new NodoArbol(nDato); // se crea el nuevo nodo
+
         if (isEmpty()) {
             root = nuevoNodo;
+            System.out.println("se creo la raiz del arbol");
         }else{
             BuscarAgregar(nuevoNodo,root);
         }
     }
 
-    public void eliminar(NodoArbol nodoBuscado){
+    public void eliminar(){
+        Scanner consola = new Scanner(System.in);
+        NodoArbol nodoBuscado;
+        int datoBuscado; // nuevo dato
+        System.out.println("ingrese el dato del nodo a eliminar");
+        datoBuscado = consola.nextInt();
+        nodoBuscado = new NodoArbol(datoBuscado); // esto se va a arreglar para que busque directamente el dato
+
         if (isEmpty()) {
             System.out.println("no hay nodos en el arbol");
         }else{
-
+            buscarEliminar(nodoBuscado, root, null);
         }
+    }
+
+    public void leerArbol(NodoArbol nActual){
+        if (isEmpty()) {
+            System.out.println("no hay nodos en el arbol para mostrar");
+            return;
+        }
+
+        if (nActual.leftEmpty()) {
+            System.out.println(nActual.getDato());
+        }else{
+            leerArbol(nActual.getLelft());
+            System.out.println(nActual.getDato());
+
+            if (nActual.rightEmpty() == false) {
+                leerArbol(nActual.getRight());
+            }
+        }
+
     }
 
     // esta funcion va a ubicar el nuevo nodo en la posicion que le corresponde del arbol
@@ -63,7 +100,7 @@ public class ArbolAVL {
         }
     }
 
-    public void buscarEliminar(NodoArbol nodoBuscado, NodoArbol nActual, NodoArbol nodoPadre){
+    private void buscarEliminar(NodoArbol nodoBuscado, NodoArbol nActual, NodoArbol nodoPadre){
         if (nActual == null) {
             System.out.println("no se encontraron coincidencias");
             return;
@@ -74,12 +111,37 @@ public class ArbolAVL {
             buscarEliminar(nodoBuscado, nActual.getRight(), nActual);
 
         }else{
+            NodoArbol nodoMenorDerecho; // nodo que se usa para identificar el nodo menor del subarbo derecho
+
             if (nodoPadre == null) { // pregunta si el nodo a eliminar es la raiz
                 // pregunto si es el nodo raizy si no tiene hijos
                 if (nActual.leftEmpty() && nActual.rightEmpty()) {
                     root = null;
                     System.out.println("se eliminaron todos los nodos del arbol");
                 }
+
+                // pregunto si el nodo raiz tiene un hijo a la derecha
+                if ((nActual.leftEmpty() == true) && (nActual.rightEmpty() == false)) {
+                    root = nActual.getRight();
+                    System.out.println("se elimino el nodo raiz");
+                }
+
+                // pregunto si el nodo raiz tiene un hijo a la izquierda
+                if ((nActual.leftEmpty() == false) && (nActual.rightEmpty() == true)) {
+                    root = nActual.getLelft();
+                    System.out.println("se elimino el nodo raiz");
+                }
+
+                // pregunto si el nodo raiz tiene 2 hijos
+                if ((nActual.leftEmpty() == false) && (nActual.rightEmpty() == false)) {
+                    // asigno el subarbol izquierdo al nodo menor de la derecha
+                    nodoMenorDerecho = nodoMenor(nActual.getRight());
+                    nodoMenorDerecho.setLeft(nActual.getLelft());
+                    // borro el nodo raiz
+                    root = nActual.getRight();
+                    System.out.println("se borro el nodo raiz");
+                }
+
               // pregunto si el nodo a borrar es un nodo terminal
             }else if (nActual.rightEmpty() && nActual.leftEmpty()) {
                 if (nActual.getDato() > nodoPadre.getDato()) {
@@ -109,7 +171,7 @@ public class ArbolAVL {
 
             }else{ // si no es ninguno de los casos anteriores, es que tiene 2 hijos
                 // el nodo menor del subarbol derecho
-                NodoArbol nodoMenorDerecho = nodoMenor(nActual.getRight());
+                nodoMenorDerecho = nodoMenor(nActual.getRight());
                 // cuelgo el subarbol izquierdo en el menor nodo del subarbolderecho
                 nodoMenorDerecho.setLeft(nActual.getLelft()); 
 
