7faff7d22772771326b7e15b1ea0a9d7ec7266ea
Daniel Olguin <danieldavidolguin@gmail.com>
2024-11-08 16:03:35 -0300

    Push previo a merge

--- ProyectoJava/Arbol_Rojinegro/src/utils/Tree.java
+++ ProyectoJava/Arbol_Rojinegro/src/utils/Tree.java
@@ -5,7 +5,7 @@ public abstract class Tree {
     public void printTree(TreeNode node, String space){
         if(node == null) return;
         
-        space += "  ";
+        space += "      ";
         printTree(node.getRight(), space);
         System.out.println(space + node.getData() + " " + node.getColor());
         printTree(node.getLeft(), space);
